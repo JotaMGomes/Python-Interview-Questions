c21e6870c2e6cc9513c1e8b4c09ec507d5f941ce
JotaMGomes <jotamgomes@gmail.com>
2020-06-04 12:24:59 -0700

    Bulls-and-cows 1

--- LeetCode/Bulls-and-cows.py
+++ LeetCode/Bulls-and-cows.py
@@ -1,31 +1,39 @@
 # https://leetcode.com/problems/bulls-and-cows/
 
-from typing import List
-
 class Solution:
   def getHint(self, secret: str, guess: str) -> str:
+    # define lists to keep char positions
     bull = []
     cow  = []
-    p = 0
+
+    # iteract through chars of secret
     for p in range(len(secret)):
+      # test is there is a match in char position
       if secret[p] == guess[p]:
+        # char found at the right position: add bull list
         bull.append(p)
       else:
+        # try to find char secret[p] in guess
         i = guess.find(secret[p])
         if i > -1:
+          # verity if this the position found is already in bull, cow, or if there is a match char in that position
           findNext = (i in bull) or (i in cow) or (secret[i] == guess[i])
           if findNext:
+            # keep looking for another position for the same char
             while findNext:
               i = guess.find(secret[p],i+1)
               if i > -1:
                 findNext = (i in bull) or (i in cow) or (secret[i] == guess[i])
                 if not findNext:
+                  # char found, but in wrong position: add cow list
                   cow.append(i)
               else:
+                # char not found
                 findNext = False
           else:
+            # char found, but in wrong position: add cow list
             cow.append(i)
-
+    # return final string
     return(str(len(bull))+"A"+str(len(cow))+"B")
  
 
