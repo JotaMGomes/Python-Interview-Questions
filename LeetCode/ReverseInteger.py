acb34e1884baf2bd2546cf97a502b56db3452f9c
JotaMGomes <jotamgomes@gmail.com>
2020-06-13 10:02:17 -0700

    Reverse Integer 1

---
+++ LeetCode/ReverseInteger.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/reverse-integer/
+# Jose Luiz Mattos Gomes
+
+from typing import List
+
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    
+    pass
+
+
+s = Solution()
+
+vNums01 = [2, 7, 11, 15]
+vTarget01 = 9
+print(s.twoSum(vNums01, vTarget01))
+
+vNums01 = [2, 7, 11, 15]
+vTarget01 = 26
+print(s.twoSum(vNums01, vTarget01))
+
+vNums01 = []
+vTarget01 = 0
+print(s.twoSum(vNums01, vTarget01))
